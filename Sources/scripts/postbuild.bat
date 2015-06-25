648623b543c9bdbb2e4c4e73ea88f83dfb9b1d52
Kiryanov Peter <kiryanov@t8.ru>
2015-06-25 11:39:19 +0300

    поменял версию и класс устройства, добавил удаление промежуточного файла S19

--- Sources/scripts/postbuild.bat
+++ Sources/scripts/postbuild.bat
@@ -1,4 +1,6 @@
 cd /d %1
 
-python.exe %1\Sources\scripts\bincopy.py cat %1\Release\Exe\Dnepr.s19 --output %1\Release\Exe\Dnepr.s20
-python.exe %1\Sources\scripts\release_postbuild.py -v %1\Sources\Profile\Generated\value.c -c DNEP0020 -i %1\Release\Exe\Dnepr.s20
+python.exe %1\Sources\scripts\bincopy.py cat %1\Release\Exe\Dnepr.s19 --output %1\Release\Exe\Dnepr_wide.s19
+python.exe %1\Sources\scripts\release_postbuild.py -v %1\Sources\Profile\Generated\value.c -c DNEP0020 -i %1\Release\Exe\Dnepr_wide.s19
+del %1\Release\Exe\Dnepr_wide.s19
+
